82b1a22e019ec35c942a784c9746f9b8dc2ae403
legion13 <legion13tr@gmail.com>
2021-01-28 01:43:31 +0300

    28.01

---
+++ 1.3.sql
@@ -0,0 +1,36 @@
+﻿create table friends (
+  id INTEGER,
+  name TEXT,
+  birthday DATE
+);
+insert into friends (id,name,birthday)
+values
+(1,'Ororo Munroe', '1940-05-30'),
+(2,'legi0n 13' , '2001-01-27'),
+(3,'John Doe', '1983-12-13');
+
+update friends 
+set name = 'Storm'
+where id =1;
+
+alter table friends
+add column email text;
+
+update friends 
+set email = 'storm@codecademy.com'
+where id =1;
+
+update friends 
+set email = 'legi0n13tr@gmail.com'
+where id =2;
+
+update friends 
+set email = 'johndoe@icloud.com'
+where id =3;
+
+delete from friends
+where id =1;
+
+
+
+select * from friends;
---
+++ 4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define variables
+            string destination = "Neptune";
+            string galaxyString = "8";
+            int galaxyInt;
+            string welcomeMessage;
+            bool outcome;
+
+            // Call DecoratePlanet() and TryParse() here
+            welcomeMessage = DecoratePlanet(destination);
+            outcome = Int32.TryParse(galaxyString, out galaxyInt);
+
+            // Print results
+            Console.WriteLine(welcomeMessage);
+            Console.WriteLine($"Parsed to int? {outcome}: {galaxyInt}");
+
+        }
+
+        // Define a method that returns a string
+        static string DecoratePlanet(string planet)
+        {
+            return $"*..*..* Welcome to {planet} *..*..*";
+        }
+
+        // Define a method with out
+        static string Whisper(string phrase, out bool wasWhisperCalled)
+        {
+            wasWhisperCalled = true;
+            return phrase.ToLower();
+        }
+    }
+}
